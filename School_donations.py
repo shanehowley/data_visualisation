c974def1865b990f2e4c230cda0e1d58597af4ee
shanehowley <snhowley@gmail.com>
2017-02-27 16:10:58 +0000

    first commit

--- School_donations.py
+++ School_donations.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
-COLLECTION_NAME = 'projects'
+MONGODB_URI = os.getenv('MONGODB_URI')
+
+# MONGODB_HOST = 'localhost'
+# MONGODB_PORT = 27017
+
+DBS_NAME = os.getenv('MONGO_DB_NAME','donorsUSA')
+COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME', 'projects')
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
 
@@ -20,9 +24,11 @@ def index():
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGODB_URI)
+
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=55000)
+
     json_projects = []
     for project in projects:
         json_projects.append(project)
